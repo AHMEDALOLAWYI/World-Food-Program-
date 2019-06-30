342576ef6b96ec7af41c4c959580d53d62bd76d5
Saqib Shamsi <shamsi.saqib@gmail.com>
2019-06-30 23:46:31 +0530

    Added discriminator, dataloaders and basic skeleton for training

--- task9_srres/srgan_pytorch/models.py
+++ task9_srres/srgan_pytorch/models.py
@@ -3,7 +3,7 @@ from torch import nn
 import model_utils
 from collections import OrderedDict
 
-DEVICE = 'cuda'
+DEVICE = 'cpu'
 
 
 class ResBlock(nn.Module):
@@ -21,6 +21,23 @@ class ResBlock(nn.Module):
         z = self.conv2(self.prelu2(self.bn2(z)))
         z = x + z
         return z
+
+
+class ConvBNLRelu(nn.Module):
+    def __init__(self, inf, outf, kernel_size=3, stride=1, neg_slope=0.2):
+        super().__init__()
+
+        self.conv = nn.Conv2d(inf, outf, kernel_size, stride, 1)
+        model_utils.w_init([self.conv])
+        self.bn = nn.BatchNorm2d(outf)
+        model_utils.g_init([self.bn])
+        self.lrelu = nn.LeakyReLU(neg_slope, inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.lrelu(x)
+        return x
 
 
 class GeneratorBNFirst(nn.Module):
@@ -76,11 +93,60 @@ class GeneratorBNFirst(nn.Module):
         return trunk
 
 
+class Discriminator(nn.Module):
+    def __init__(self, n_planes, image_dims, neg_slope=0.2, sigmoid=True):
+        super().__init__()
+
+        self.first_conv = nn.Conv2d(3, n_planes, 3, 1, 1)
+        model_utils.w_init([self.first_conv])
+        self.lrelu = nn.LeakyReLU(neg_slope)
+
+        self.layer1 = ConvBNLRelu(n_planes, n_planes, stride=2, neg_slope=neg_slope)  # 1/2
+        self.layer2 = ConvBNLRelu(n_planes, n_planes * 2, stride=1, neg_slope=neg_slope) # 1/2
+        self.layer3 = ConvBNLRelu(n_planes * 2, n_planes * 2, stride=2, neg_slope=neg_slope) # 1/4
+        self.layer4 = ConvBNLRelu(n_planes * 2, n_planes * 4, stride=1, neg_slope=neg_slope) # 1/4
+        self.layer5 = ConvBNLRelu(n_planes * 4, n_planes * 4, stride=2, neg_slope=neg_slope) # 1/8
+        self.layer6 = ConvBNLRelu(n_planes * 4, n_planes * 8, stride=1, neg_slope=neg_slope) # 1/8
+        self.layer7 = ConvBNLRelu(n_planes * 8, n_planes * 8, stride=2, neg_slope=neg_slope) # 1/16
+        self.fc = nn.Linear((image_dims // 16) * (image_dims // 16) * n_planes * 8, 100)
+        self.out = nn.Linear(100, 1)
+        self.sigmoid = sigmoid
+
+    def forward(self, x):
+        x = self.first_conv(x)
+        x = self.lrelu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.layer7(x)
+
+        # Flatten
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        x = self.out(x)
+
+        if self.sigmoid:
+            x = torch.sigmoid(x)
+        return x
+
+
 if __name__ == '__main__':
-    # Test architecture
-    gen = GeneratorBNFirst(3, 3, upscale=4)
-    gen.to(DEVICE)
-    # print(gen)
-    img = torch.rand(1, 3, 256, 256).to(DEVICE)
-    out = gen(img)
-    print(out.shape)
+    # Test generator
+    # gen = GeneratorBNFirst(3, 3, upscale=4)
+    # gen.to(DEVICE)
+    # # print(gen)
+    # img = torch.rand(1, 3, 256, 256).to(DEVICE)
+    # out = gen(img)
+    # print(out.shape)
+
+    # Test discriminator
+    img_dims = 512
+    dis = Discriminator(64, 512)
+    dis.to(DEVICE)
+    img = torch.rand(1, 3, 512, 512)
+    out = dis(img)
+    print(out)
