3b35954a5cd7755b84fbedbb1d2ab6f09ccf1cd2
Erick <erick.galinkin@gmail.com>
2019-07-06 07:27:34 -0700

    Updated model - WIP.

--- task7_MaskRCNN/model.py
+++ task7_MaskRCNN/model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Omdena Mask R-CNN for UN World Food Program
+# Omdena Mask Regional Convolutional Neural Network (Mask R-CNN) for UN World Food Program
 # Written by Erick Galinkin
 # Note: I've opted to use Tensorflow 2.0 here. It should be easy to change if need be, but I'm assuming
 # that support for TF2 will be longer-lived, and we get to play with all of the Keras abstractions without having to
@@ -7,7 +7,7 @@
 
 import tensorflow as tf
 import numpy as np
-from tensorflow.python.keras.layers import Dense, Flatten, Conv2D, BatchNormalization, Add, Activation
+from tensorflow.python.keras.layers import Flatten, Conv2D, BatchNormalization, Add, Activation, ZeroPadding2D, MaxPool2D
 from tensorflow.python.keras import Model
 
 data_dir = './images/'
@@ -17,7 +17,7 @@ data_dir = './images/'
 class MaskRCNN(Model):
     def __init__(self):
         super(MaskRCNN, self).__init__()
-        self.conv1 = Conv2D(256, (1, 1), use_bias=True, activation='relu')
+        self.conv1 = Conv2D(256, (1, 1), use_bias=True)
         self.conv2 = Conv2D(128, (3, 3), padding='same', use_bias=True)
         self.conv3 = Conv2D(64, (1, 1), use_bias=True)
         self.bn_train = BatchNormalization(trainable=True)
@@ -26,40 +26,76 @@ class MaskRCNN(Model):
         self.add = Add()
         self.activation = Activation('relu')
 
-    # Heavy inspiration from Matterport's Mask RCNN implementation.
-    def identity_block(self, input_tensor):
-        x = self.conv1(input_tensor)
-        x = self.bn_train(x, training=train_bn)
-        x = self.activation(x)
-        x = self.conv2(x)
-        x = self.bn_train(x)
-        x = self.activation(x)
-        x = self.conv3(x)
-        x = self.add([x, input_tensor])
-        x = self.activation(x)
+
+# It may be prudent to rip this whole thing out and use Thomas's pre-trained Resnet-50
+# Heavy inspiration from Matterport's Mask RCNN implementation.
+class Backbone(Model):
+    def __init__(self):
+        super(Backbone, self).__init__()
+        self.conv_1 = Conv2D(64, (7, 7), strides=(2, 2), use_bias=True)
+        self.batchnorm = BatchNormalization(trainable=True)
+        self.maxpool = MaxPool2D((3, 3), strides=(2, 2), padding="same")
+        self.zero_pad = ZeroPadding2D((3,3))
+        self.activation = Activation('relu')
+
+    @staticmethod
+    def conv_block(input_tensor, kernel_size, filters, use_bias=True, trainable=True):
+        filter1, filter2, filter3 = filters
+
+        x = Conv2D(filter1, (1, 1), strides=(2, 2), use_bias=use_bias)(input_tensor)
+        x = BatchNormalization()(x, trainable=trainable)
+        x = Activation('relu')(x)
+        x = Conv2D(filter2, (kernel_size, kernel_size), padding='same', use_bias=use_bias)(x)
+        x = BatchNormalization()(x, trainable=trainable)
+        x = Activation('relu')(x)
+        x = Conv2D(filter3, (1, 1), use_bias=use_bias)(x)
+        x = BatchNormalization()(x, trainable=trainable)
+        shortcut = Conv2D(filter3, (1, 1), strides=(2, 2), use_bias=True)(input_tensor)
+        shortcut = BatchNormalization()(shortcut, trainable=trainable)
+        x = Add()([x, shortcut])
+        x = Activation('relu')(x)
         return x
 
-    def conv_block(self, input_tensor):
-        x = self.conv1(input_tensor)
-        x = self.bn_train(x)
-        x = self.activation(x)
-        x = self.conv2(x)
-        x = self.bn_train(x)
-        x = self.activation(x)
-        x = self.conv3(x)
-        x = self.bn_train(x)
-        shortcut = conv3(input_tensor)
-        shortcut = self.bn_train(shortcut)
-        x = self.add([x, shortcut])
-        x = self.activation(x)
+    @staticmethod
+    def identity_block(input_tensor, kernel_size, filters, use_bias=True, trainable=True):
+        filter1, filter2, filter3 = filters
+
+        x = Conv2D(filter1, (1, 1), use_bias=use_bias)(input_tensor)
+        x = BatchNormalization()(x, trainable=trainable)
+        x = Activation('relu')(x)
+        x = Conv2D(filter2, (kernel_size, kernel_size), padding='same', use_bias=True)(x)
+        x = BatchNormalization()(x, trainable=trainable)
+        x = Activation('relu')(x)
+        x = Conv2D(filter3, (1, 1), use_bias=True)(x)
+        x = BatchNormalization()(x, trainable=trainable)
+        x = Add()([x, input_tensor])
+        x = Activation('relu')(x)
         return x
 
+    def resnet_50(self, channels, input_image, trainable):
+        x = self.zero_pad(input_image)
+        x = self.conv_1(x)
+        x = self.batchnorm(x)
+        x = self.activation(x)
+        C1 = x = self.maxpool(x)
+        x = self.conv_block(x, 3, [64, 64, 256], trainable=trainable)
+        x = self.identity_block(x, 3, [64, 64, 256], trainable=trainable)
+        C2 = x = self.identity_block(x, 3, [64, 64, 256], trainable=trainable)
+        x = self.conv_block(x, 3, [128, 128, 512], trainable=trainable)
+        x = self.identity_block(x, 3, [128, 128, 512], trainable=trainable)
+        x = self.identity_block(x, 3, [128, 128, 512], trainable=trainable)
+        C3 = x = self.identity_block(x, 3, [128, 128, 512], trainable=trainable)
+        x = self.conv_block(x, 3, [256, 256, 1024], trainable=trainable)
+        # Per Matterport implementation, if we wanted to change this to resnet 101, we'd have 22 instead of 5 blocks
+        x = self.identity_block(x, 3, [256, 256, 1024], trainable=trainable)
+        x = self.identity_block(x, 3, [256, 256, 1024], trainable=trainable)
+        x = self.identity_block(x, 3, [256, 256, 1024], trainable=trainable)
+        x = self.identity_block(x, 3, [256, 256, 1024], trainable=trainable)
+        x = self.identity_block(x, 3, [256, 256, 1024], trainable=trainable)
+        C4 = x
+        # Skipping stage 5 entirely - if we decide we need it, we can always come back and add it.
+        return [C1, C2, C3, C4]
 
-    def call(self, x):
-        x = self.conv1(x)
-        x = self.flatten(x)
-        x = self.d1(x)
-        return self.d2(x)
 
 
 # There is no labeled data yet, so this is more or less just a mock-up. Once we have data, we'll read it in from a
